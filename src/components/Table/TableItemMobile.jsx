ba582b184ddc20ce3e3c9a07add8aaaa957c5e39
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-04-30 22:59:21 -0700

    tweaked mobile sizing

--- src/components/Table/TableItemMobile.jsx
+++ src/components/Table/TableItemMobile.jsx
@@ -56,9 +56,9 @@ const TableItemMobile = ({ data }) => {
                         highlighting === "F2L" ? "&stage=f2l" : ""
                     }&case=${convertAlg(data.alg)}`}
                     alt={data.name}
-                    className="w-[35%] max-w-40"
+                    className="w-[35%] md:max-w-40"
                 />
-                <div className="w-full text-center border border-gray-600 h-full flex items-center justify-center">
+                <div className="w-[65%] md:w-full text-center border border-gray-600 h-full flex items-center justify-center">
                     {data.alg}
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     theme: {
         extend: {},
         screens: {
+            md: "768px",
             lg: "1025px",
         },
     },
